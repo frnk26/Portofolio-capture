ab7476188556932df42d5be435356c72f3be93a1
unknown <frankahlennn@gmail.com>
2022-11-22 08:50:55 +0800

    creating Reusable toggle component for Questionand Answer using useState and frame-motion on FAQ section

---
+++ src/components/Toggle.jsx
@@ -0,0 +1,16 @@
+import { motion as m } from "framer-motion";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Toggle = ({ title, children }) => {
+  const [toggle, setToggle] = useState(true);
+  return (
+    <m.div layout className="question" onClick={() => setToggle(!toggle)}>
+      <m.h4 layout>{title}</m.h4>
+      {toggle ? children : ""}
+      <m.div className="faq-line"></m.div>
+    </m.div>
+  );
+};
+
+export default Toggle;
