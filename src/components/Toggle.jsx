c451479f1ba7a25a9e5687e569da03ae7d807280
unknown <frankahlennn@gmail.com>
2022-11-22 16:21:08 +0800

    save point

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 const Toggle = ({ title, children }) => {
-  const [toggle, setToggle] = useState(true);
+  const [toggle, setToggle] = useState(false);
   return (
     <m.div layout className="question" onClick={() => setToggle(!toggle)}>
       <m.h4 layout>{title}</m.h4>
